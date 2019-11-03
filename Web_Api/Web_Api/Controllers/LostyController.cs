9a19acd323ab82ddb20c499ea475212279827468
sari56 <55346748+sari56@users.noreply.github.com>
2019-11-04 00:34:04 +0200

    commit api
    commit the new api

---
+++ Web_Api/Web_Api/Controllers/LostyController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Web_Api.Models;
+
+namespace Web_Api.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class LostyController : ApiController
+    {
+        public Connection connection = new Connection();
+
+        //[Route("http://localhost:59282/api/Home/GetCity")]
+        [HttpPost]
+        public List<City> GetCity()
+        {
+            SqlCommand cmd = ConnectSql("Select * from City");
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<City> resultCity = new List<City>();
+            while (reader.Read())
+            {
+                City city = new City()
+                {
+                    CityCode = (int)reader["CityCode"],
+                    CityName = reader["CityName"].ToString(),
+                    CityTavCode = reader["CityTavCode"].ToString(),
+                };
+                var cityName = reader["CityName"];
+                resultCity.Add(city);
+            }
+            connection.DisConnectSql();
+            int x = resultCity.Count();
+            return resultCity;
+        }
+
+        public SqlCommand ConnectSql(string query)
+        {
+            return connection.connectToSql(query);
+        }
+    }
+}
