bd7a37bf140485361b803ee1a0c4a2ea440380ee
sari56 <55346748+sari56@users.noreply.github.com>
2019-11-06 23:06:17 +0200

    Add_Function

--- Web_Api/Web_Api/Controllers/LostyController.cs
+++ Web_Api/Web_Api/Controllers/LostyController.cs
@@ -38,6 +38,82 @@ namespace Web_Api.Controllers
             return resultCity;
         }
 
+        [Route("api/Losty/GetCategory")]
+        [HttpPost]
+        public List<Category> GetCategory()
+        {
+            SqlCommand cmd = ConnectSql("Select * from Category");
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Category> resultCategory = new List<Category>();
+            while (reader.Read())
+            {
+                Category category = new Category()
+                {
+                    CategoryCode = (int)reader["CategoryCode"],
+                    CategoryDesc = reader["CategoryDesc"].ToString(),
+
+                };
+                resultCategory.Add(category);
+            }
+            connection.DisConnectSql();
+            var cnt = resultCategory.Count();
+            return resultCategory;
+        }
+
+        [Route("api/Losty/GetFounds")]
+        [HttpPost]
+        public List<Found> GetFounds(int @CategoryCode, string @LossColor, DateTime @LossDate)
+        {
+            SqlCommand cmd = ConnectSql("Select * From Found Where ((@Category=CategoryCode AND @LossColor = FoundColor AND @LossDate < FoundDate) OR (@CategoryCode = CategoryCode AND @FoundColor = FoundColor) OR (@CategoryCode = CategoryCode @FoundDate < FoundDate) OR ( @LossColor = FoundColor AND @LossDate < FoundDate))");
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Found> resultFounds = new List<Found>();
+            while (reader.Read())
+            {
+                Found found = new Found()
+                {
+                    FoundCode = (int)reader["FoundCode"],
+                    FindID = reader["FindID"].ToString(),
+                    CategoryCode = (int)reader["CategoryCode"],
+                    FoundColor = reader["FoundColor"].ToString(),
+                    FoundDate = (DateTime)reader["FoundDate"],
+
+                };
+                resultFounds.Add(found);
+            }
+            connection.DisConnectSql();
+            if (resultFounds.Count() == 0)
+                return null;
+            return resultFounds;
+        }
+
+        [Route("api/Losty/InsetUser")]
+        [HttpPost]
+        public bool InsetUser(Person person)
+        {
+            SqlCommand cmd = ConnectSql("Insert into Person values (@PersonId,@PersonName,@PersonCityCode,@PersonAddress,@PersonPhone,@personEmail)"
+             );
+            try
+            {
+                cmd.Parameters.AddWithValue("@PersonId", person.PersonID.ToString());
+                cmd.Parameters.AddWithValue("@PersonName", person.PersonName.ToString());
+                cmd.Parameters.AddWithValue("@PersonCityCode", person.PersonCityCode);
+                cmd.Parameters.AddWithValue("@PersonAddress", person.PersonAddress.ToString());
+                cmd.Parameters.AddWithValue("@PersonPhone", person.PersonPhone.ToString());
+                cmd.Parameters.AddWithValue("@personEmail", person.PersonEmail.ToString());
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.DisConnectSql();
+            }
+
+        }
+
         public SqlCommand ConnectSql(string query)
         {
             return connection.connectToSql(query);
